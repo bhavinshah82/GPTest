931a9790464b9a4e586a1bc769ac09d8aaca4d35
bhavinshah <bhavinshah1982@gmail.com>
2016-05-27 22:41:45 +0530

    added other REST api's put, post, delete for notes

--- notes/unotes/src/main/java/org/bshah/gp/hibernate/NotesBO.java
+++ notes/unotes/src/main/java/org/bshah/gp/hibernate/NotesBO.java
@@ -8,12 +8,18 @@ import org.bshah.gp.entity.Notes;
 import org.bshah.gp.entity.User;
 import org.bshah.gp.utils.HibernateUtils;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
-public class NotesBO 
+public class NotesBO
 {
+
+	public NotesBO()
+	{
+	}
+
 	@SuppressWarnings("unchecked")
 	public static List<Notes> getAllUserNotes(int userid)
 	{
@@ -71,20 +77,35 @@ public class NotesBO
 		return note;
 	}
 
-	public static int addUserNote(int id, Notes note)
+	@SuppressWarnings("unchecked")
+	public static void updateUserNote(int userid, int noteid, Notes updatedNote)
 	{
-		User user = UserBO.getUser(id);
-		note.setUser(user);
-		Date date = new Date();
-		note.setCreationTime(date);
-		note.setLastUpdateTime(date);
-		int noteid = -1;
+		System.out.println("Updating user note id = " +noteid);
 		Session session = null;
+		List<Notes> notesList = new ArrayList<Notes>();
+		Notes note = null;
 		try
 		{
 			session = HibernateUtils.openSession();
 			Transaction tx = session.beginTransaction();
-			noteid=(int) session.save(note);
+			User user = UserBO.getUser(userid);
+			notesList = (ArrayList<Notes>) session.createCriteria(Notes.class).add(Restrictions.eq("user", user))
+					.add(Restrictions.eq("noteId", noteid)).list();
+			System.out.println("noteslist size = "+notesList.size());
+
+			if (notesList.size() == 0)
+			{
+				// throw new Exception("Unauthorized");
+			}
+			else
+			{
+				note = notesList.get(0);
+				System.out.println("Updating note - "+note.getNoteId());
+				note.setLastUpdateTime(new Date());
+				note.setNote(updatedNote.getNote());
+				note.setTitle(updatedNote.getTitle());
+				session.update(note);
+			}
 			tx.commit();
 		}
 		catch (HibernateException e)
@@ -97,7 +118,89 @@ public class NotesBO
 		{
 			HibernateUtils.closeSession(session);
 		}
-		return noteid;
+	}
+
+	public static void addUserNote(int id, Notes note)
+	{
+		User user = UserBO.getUser(id);
+		note.setUser(user);
+		Date date = new Date();
+		note.setCreationTime(date);
+		note.setLastUpdateTime(date);
+		Session session = null;
+		try
+		{
+			session = HibernateUtils.openSession();
+			Transaction tx = session.beginTransaction();
+			session.save(note);
+			tx.commit();
+		}
+		catch (HibernateException e)
+		{
+			session.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		}
+		finally
+		{
+			HibernateUtils.closeSession(session);
+		}
+	}
+
+	public static void deleteAllNotes(int id)
+	{
+		Session session = null;
+		try
+		{
+			session = HibernateUtils.openSession();
+			session.beginTransaction();
+			String hql = "delete from Notes where userId = " + id;
+			Query query = session.createQuery(hql);
+			query.executeUpdate();
+		}
+		catch (HibernateException e)
+		{
+			session.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		}
+		finally
+		{
+			HibernateUtils.closeSession(session);
+		}
+	}
+
+	public static void deleteNote(int userid, int noteid)
+	{
+		Session session = null;
+		try
+		{
+			session = HibernateUtils.openSession();
+			Transaction tx = session.beginTransaction();
+			User user = UserBO.getUser(userid);
+			Notes note = (Notes) session.get(Notes.class, noteid);
+			@SuppressWarnings("unchecked")
+			ArrayList<Notes> notesList = (ArrayList<Notes>) session.createCriteria(Notes.class)
+					.add(Restrictions.eq("user", user)).add(Restrictions.eq("noteId", noteid)).list();
+
+			if (notesList.size() == 0)
+			{
+				return;
+			}
+			note = notesList.get(0);
+			session.delete(note);
+			tx.commit();
+		}
+		catch (HibernateException e)
+		{
+			session.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		}
+		finally
+		{
+			HibernateUtils.closeSession(session);
+		}
 	}
 
 }
--- notes/unotes/src/main/java/org/bshah/gp/rest/NotesAPI.java
+++ notes/unotes/src/main/java/org/bshah/gp/rest/NotesAPI.java
@@ -3,7 +3,10 @@ package org.bshah.gp.rest;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -13,11 +16,11 @@ import org.bshah.gp.entity.Notes;
 import org.bshah.gp.hibernate.NotesBO;
 
 @Path("user/{userid}/notes")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class NotesAPI 
 {
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
 	public List<Notes> getAllUserNotes(@PathParam("userid") int userid)
 	{
 		List<Notes> notesList = NotesBO.getAllUserNotes(userid);
@@ -30,5 +33,33 @@ public class NotesAPI
 	{
 		return NotesBO.getNote(userid, noteid);
 	}
+
+	@POST
+	public void addUserNote(@PathParam("userid") int userid, Notes note)
+	{
+		NotesBO.addUserNote(userid, note);
+	}
+	
+	@DELETE
+	public void deleteAllUserNotes(@PathParam("userid") int userid)
+	{
+		NotesBO.deleteAllNotes(userid);
+	}
+	
+
+	@DELETE
+	@Path("/{noteid}")
+	public void deleteUserNote(@PathParam("userid") int userid, @PathParam("noteid") int noteid)
+	{
+		NotesBO.deleteNote(userid, noteid);
+	}
+	
+	@PUT
+	@Path("/{noteid}")
+	public void updateNote(@PathParam("userid") int userid, @PathParam("noteid") int noteid, Notes updatedNote) 
+	{
+		NotesBO.updateUserNote(userid, noteid, updatedNote);
+	}
+
 
 }
